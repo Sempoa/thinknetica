7890d08cce431eeefacc24f42caa789f949e49c7
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-04-22 22:53:18 +0300

    update problem1.rb 2.0

--- problem1.rb
+++ problem1.rb
@@ -15,19 +15,24 @@ class Station
     self.trains.delete(train)
   end
 
-
-  def trains_type
-    self.trains.each { |train| puts train.type }
+  def trains_type(type)
+    list = Array.new
+    self.trains.each do |train|
+      if train.type == type
+        list.append(train)
+      end
+    end
+    return list
   end
+
 end
 
 class Route
+
   attr_reader :stations
 
   def initialize(first_station, last_station)
-    @stations = []
-    @stations << first_station
-    @stations << last_station
+    @stations = [first_station, last_station]
   end
 
   def add_station(station)
@@ -37,60 +42,108 @@ class Route
   def del_station(station)
     @stations.delete(station)
   end
+
+  def first_station
+    @stations[0].name
+  end
+  def last_station
+    @stations[-1].name
+  end
+
+  def all_stations
+    list = Array.new
+    self.stations.each do |station|
+        list.append(station.name)
+    end
+    return list
+  end
+
 end
 
 class Train
-  attr_reader :number, :type , :route
-  attr_accessor :station, :speed, :count
+  attr_reader :number, :type , :route, :station, :speed, :count_wag, :station_index
 
   def initialize(number, type, count_wag)
     @number = number
     @type = type
-    @count = count
+    @count_wag = count_wag
     @speed = 0
   end
 
-  def incr_speed(speed)
-    self.speed += speed
+  def plus_speed(speed)
+    @speed += speed
   end
 
-  def stop
-    self.speed = 0
+  def minus_speed(speed)
+    if @speed >= speed
+      @speed -= speed
+    else
+      "Wrong speed"
+    end
   end
 
   def add_wag
-    if self.speed == 0
-      self.count_wag += 1
+    if @speed == 0
+      @count_wag += 1
     else
-      puts "Stop the train"
+      "Stop the train"
     end
   end
 
   def del_wag
-    if self.speed == 0
-      self.count_wag -= 1
+    if @count_wag > 0
+      if @speed == 0
+        @count_wag -= 1
+      else
+        "Stop the train"
+      end
     else
-      puts "Stop the train"
+      "No wagons"
     end
   end
 
   def route=(route)
     @route = route
-    self.station = self.route.stations.first
+    @station = self.route.stations.first
+    @station.get_train(self)
+    @station_index = self.route.stations.index(@station)
   end
 
   def next_station
-    self.route.stations[self.route.stations.index(self.station) + 1].name
+    if @station_index != -1
+      self.route.stations[@station_index + 1]
+    else
+      "You are at the last station"
+    end
   end
 
   def prev_station
-    self.route.stations[self.route.stations.index(self.station) - 1].name
+    if @station_index != 0
+      self.route.stations[@station_index - 1]
+    else
+      "You are at the first station"
+    end
   end
 
   def move_next
-    self.station = self.route.stations[self.route.stations.index(self.station) + 1]
+    if @station_index != -1
+      @station.send_train(self)
+      @station = self.route.stations[@station_index += 1]
+      @station.get_train(self)
+    else
+      "Last station"
+    end
   end
+
   def move_prev
-    self.station = self.route.stations[self.route.stations.index(self.station) - 1]
+    if @station_index != 0
+      @station.send_train(self)
+      @station = self.route.stations[@station_index -= 1]
+      @station.get_train(self)
+    else
+      "First station"
+    end
   end
+
 end
+
