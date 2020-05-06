eaee23995f36aeda8c489bae324e5a74f5a3162c
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-07 01:35:27 +0200

    Add files via upload

---
+++ cargo-wagon.rb
@@ -0,0 +1,7 @@
+class CargoWagon < Wagon
+
+  def initialize(number, type = 'cargo')
+    super
+  end
+      
+end
---
+++ wagon.rb
@@ -0,0 +1,10 @@
+class Wagon
+
+  attr_accessor :type, :number
+
+  def initialize(number, type)
+    @number = number
+    @type = type
+  end
+
+end
