d787d9df006e23eb7bdcf12ef69461d8f9ba3a2c
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-12 19:35:17 +0200

    Update station.rb

--- station.rb
+++ station.rb
@@ -1,10 +1,21 @@
+require_relative 'instance_counter'
+
 class Station
 
+  include InstanceCounter
+
   attr_accessor :name, :trains
+  
+  @@stations = []
+
+  def self.all
+    @@stations
+  end
 
   def initialize(name)
     @name = name
     @trains = []
+    @@stations << self
   end
 
   def get_train(train)
