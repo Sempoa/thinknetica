5ff3a91a35ffabb33f063bf199b649969d7aea7a
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-13 07:48:56 +0200

    Update wagon.rb

--- wagon.rb
+++ wagon.rb
@@ -1,10 +1,17 @@
+require_relative 'factory'
+require_relative 'instance_counter'
+
 class Wagon
+
+  include Factory
+  include InstanceCounter
 
   attr_accessor :type, :number
 
   def initialize(number, type)
     @number = number
     @type = type
+    register_instance
   end
 
 end
