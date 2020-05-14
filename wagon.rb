d533e695961dbf4a7eb64b7703b44159e2c64178
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-14 23:31:15 +0200

    Update wagon.rb

--- wagon.rb
+++ wagon.rb
@@ -1,10 +1,22 @@
 class Wagon
 
+  include Factory
+  include InstanceCounter
+  include Validation
+
   attr_accessor :type, :number
 
   def initialize(number, type)
     @number = number
     @type = type
+    validate!
+    register_instance
   end
 
+  protected
+
+  def validate!
+    raise "Недопустимый формат номера" if self.number.nil?
+  end
 end
+
