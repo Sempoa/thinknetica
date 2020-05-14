8fea209d63f7a97dd5823113295405d1de33c3bf
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-14 23:30:15 +0200

    Update station.rb

--- station.rb
+++ station.rb
@@ -1,10 +1,22 @@
 class Station
 
+  include InstanceCounter
+  include Validation  #не знаю, почему оно ругается именно в этом классе, хотя оба модуля, точнее файлы, подключены в main
+
   attr_accessor :name, :trains
+  
+  @@stations = []
+
+  def self.all
+    @@stations
+  end
 
   def initialize(name)
     @name = name
     @trains = []
+    validate!
+    @@stations << self
+    register_instance
   end
 
   def get_train(train)
@@ -13,6 +25,12 @@ class Station
 
   def send_train(train)
     self.trains.delete(train)
+  end
+
+  protected
+
+  def validate!
+    raise "Имя станции должно содержать хотя бы один символ" if self.name.nil?
   end
 
 end
