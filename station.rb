ffc17a25319ef835175bd8ebf533f3a98f26f93c
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-15 10:19:04 +0200

    Update station.rb

--- station.rb
+++ station.rb
@@ -1,10 +1,23 @@
 class Station
 
+  include InstanceCounter
+  include Validation
+  FORMAT = /^([a-zа-я]|\d){2,50}$/i
+
   attr_accessor :name, :trains
+  
+  @@stations = []
+
+  def self.all
+    @@stations
+  end
 
   def initialize(name)
     @name = name
     @trains = []
+    validate!
+    @@stations << self
+    register_instance
   end
 
   def get_train(train)
@@ -13,6 +26,13 @@ class Station
 
   def send_train(train)
     self.trains.delete(train)
+  end
+
+  protected
+
+  def validate!
+    raise "Имя станции должно содержать хотя бы два символа" if self.name.nil?
+    raise "Имя станции должно содержать хотя бы два символа" if self.name !~ FORMAT
   end
 
 end
