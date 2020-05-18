0b0ebb1f807188ef6229f1ca5344c2eca1c952f8
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-18 23:01:09 +0200

    Update wagon.rb

--- wagon.rb
+++ wagon.rb
@@ -5,20 +5,51 @@ class Wagon
   include Validation
   FORMAT = /^([a-zа-я]|\d){1,50}$/i
 
-  attr_accessor :type, :number
+  attr_accessor :type, :number, :value, :occupied_seats, :space_volume, :nonfree_volume, :free_seats
 
-  def initialize(number, type)
+  def initialize(number, volume, type)
     @number = number
     @type = type
     validate!
     register_instance
   end
+  #---------------------------------------------------CARGO
+  def take_volume(take)
+    if self.space_volume >= take
+      self.space_volume -= take
+      @nonfree_volume += take
+    end
+  end
 
+  def occupied_volume
+    @nonfree_volume
+  end
+
+  def free_volume
+    self.space_volume
+  end
+  #----------------------------------------------------PASS
+  def occupy_seats
+    if self.free_seats > 0
+      self.free_seats -= 1
+      @occupied_seats += 1
+    end
+  end
+
+  def number_occupied_seats
+    @occupied_seats
+  end
+
+  def number_free_seats
+    self.free_seats
+  end
+  #-----------------------------------------------VALIDATE
   protected
 
   def validate!
     raise "Недопустимый формат номера" if self.number.nil?
     raise "Недопустимый формат номера" if self.number !~ FORMAT
   end
-  
+
 end
+
