d1a2e820428af4fe2bbd66e630bd299870c08d67
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-18 23:01:54 +0200

    Update cargo-wagon.rb

--- cargo-wagon.rb
+++ cargo-wagon.rb
@@ -1,7 +1,9 @@
 class CargoWagon < Wagon
 
-  def initialize(number, type = 'cargo')
+  def initialize(number, value, type = 'cargo')
     super
+    @space_volume = value
+    @nonfree_volume = 0
   end
-      
+
 end
