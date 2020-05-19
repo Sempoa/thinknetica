00f14b471b69463194ed6d91d422eee37dd31720
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-19 16:03:36 +0200

    Update pass-wagon.rb

--- pass-wagon.rb
+++ pass-wagon.rb
@@ -1,7 +1,12 @@
 class PassWagon < Wagon
 
-  def initialize(number, type = 'passenger')
+  def initialize(number, value, type = 'passenger')
     super
   end
-  
+
+  def take_value
+    super(1)
+  end
+
 end
+
