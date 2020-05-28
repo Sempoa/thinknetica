69fee58c3189edf226f78ce5cb3ba03886f8f11d
Sempoa <62727455+Sempoa@users.noreply.github.com>
2020-05-28 03:36:41 +0300

    Update validate.rb

--- validate.rb
+++ validate.rb
@@ -1,10 +1,45 @@
 module Validation
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
 
-  def valid?
-      validate!
-      true
-  rescue
-      false
+  module ClassMethods
+    def validate(name, *args)
+      @validates ||= {}
+      @validates[name] = *args
+      puts "validate #{@validates}"
+    end
+  end
+
+  module InstanceMethods
+    def validate!
+      valid_vars = self.class.instance_variable_get("@validates")
+      puts "validate! #{valid_vars}"
+      valid_vars.each do |name, args|
+        @name_value = instance_variable_get("@#{name}")
+        @param = args[1]
+        send args[0]
+      end
+    end
+    def valid?
+        validate!
+        true
+    rescue
+        false
+    end
+
+    def presence
+      raise "Недопустимый формат аттрибута: пусто или nil" if @name_value.nil? || @name_value.empty?
+    end
+
+    def format
+      raise "Недопустимый формат аттрибута: неправильный номер или имя аттрибута" if @name_value !~ @param
+    end
+
+    def type
+      raise "Типы аттрибута и класса не совпадают" unless @name_value.is_a?(@param)
+    end
   end
 
 end
