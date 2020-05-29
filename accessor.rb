8a331401b28b6f2f1ca7a6cdda06695323052fb5
Qwerty <62727455+Sempoa@users.noreply.github.com>
2020-05-29 14:30:12 +0300

    Update accessor.rb

--- accessor.rb
+++ accessor.rb
@@ -2,7 +2,6 @@ module Accessor
 
   def attr_accessor_with_history (*names)
     names.each do |name|
-      history = []
       var_name = "@#{name}".to_sym
       var_history = "@#{name}_history".to_sym
       define_method(name) { instance_variable_get(var_name) }
@@ -12,6 +11,7 @@ module Accessor
 
       define_method("#{name}=".to_sym) do |value|
         instance_variable_set(var_name, value)
+        history = instance_variable_get(var_history) || []
         instance_variable_set(var_history, history << value)
       end
     end
